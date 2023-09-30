8a1a04e9732d8790b943c2eb0a36417331a0e707
jackcook <jack-c-cook@protonmail.com>
2023-09-29 21:10:24 -0500

    add address claim and response to request

--- scripts/r_logger.c
+++ scripts/r_logger.c
@@ -10,13 +10,49 @@ void term(int signum)
     done = 1;
 }
 
+u_int32_t get_pgn(uint32_t frame_id) {
+    // Note: pgn is a 17 bit value
+    // The data page (DP) is contained in bit 0 of the msb
+    u_int8_t data_page = (((frame_id >> 24) & 0x1f) >> 17) & 0x1;
+
+    // The PDU format (PF) is located one byte to the right of msb
+    u_int8_t pdu_format = (frame_id >> 16) & 0xFF;
+    // The PDU specific (PS) is located one byte to the right of PF
+    u_int8_t pdu_specific = (frame_id >> 8) & 0xFF;
+
+    u_int32_t pgn = data_page << 16 | pdu_format << 8;
+
+    if (pdu_format<=239) {} // PDU1 has PF from 0-239
+    else if (pdu_format >= 240) pgn |= pdu_specific; // PDU2 has PF from 240-255
+
+    return pgn;
+}
+
+void send_address_claim(struct Bus * can_bus) {
+    struct Message can_msg;
+    can_msg.arbitration_id = 0x18eefff9;
+    can_msg.dlc = 8;
+    can_msg.is_extended_id = 1;
+    can_msg.is_remote_frame = false;
+    can_msg.is_error_frame = false;
+    uint64_t data = 0x201281000cc00000;
+    for (int i=1; i<8+1; i++) {
+        uint8_t shift_right = 64 - (8 * i);
+        can_msg.data[i-1] = (data >> shift_right) & 0xff;
+    }
+
+    can_bus->methods.send(can_bus->interface, &can_msg);
+}
+
 void monitor(struct Bus * can_bus, struct RotatingLogger * r_logger) {
     struct Message msg = bus_recv(can_bus, 0.0);
 
     if (msg._recv_error == false) {
         log_msg(r_logger, &msg);
         if (can_bus->channel_idx == 2) {
-            // TODO: Check to see if there has been an address request
+            if (get_pgn(msg.arbitration_id) == 0xea00) {
+                if (((msg.data[1] << 8) | msg.data[0]) == 0xee00) send_address_claim(can_bus);
+            }
         } // fi
     } // fi
 }
@@ -50,7 +86,7 @@ int main() {
     struct RotatingLogger r_logger = create_rotating_logger(
             "file.blf", 250000, 300, (void*)&args);
 
-    // TODO: Send address claim from CAN1
+    send_address_claim(&buses[0]);
 
     while (!done) {
         for (int i=0; i<num_buses; i++) {
@@ -59,6 +95,10 @@ int main() {
     }
 
     shutdown_rotating(&r_logger);
+
+    for (int i=0; i<num_buses; i++) {
+        buses[i].methods.close(buses[i].interface);
+    }
 
     return 0;
 }
